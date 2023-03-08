a16d13a0bd804d849542008bd4e99f83f7a716a1
Valentin <valio990210@gmail.com>
2023-03-08 15:53:04 +0200

    util

--- src/api/api/api.js
+++ src/api/api/api.js
@@ -1,27 +1,38 @@
-const  host = 'http//localhost:3030'
+const host = 'http//localhost:3030'
 
-async function request(url, method, data){
+async function request(url, method, data) {
 
     const options = {
         method,
         headers: {},
     }
 
-    const res = await fetch (host + url, options);
-
-    if(res.ok == false){
-        if(res.status = 403){
-            //TODO
-        }
-        const error = await res.json();
-        throw new Error(error.message)
+    if(data != undefined){
+        options.headers['Content-Type' = 'application/json'];
+        options.body = JSON.stringify(data)
     }
 
-    //if there is data in the request status will be 200 - or 204 if no data
+    try {
+        const res = await fetch(host + url, options);
 
-    if(res.status == 204){
-        return res;
-    } else {
-        return res.json()
+        if (res.ok == false) {
+            if (res.status == 403) {
+                //TODO
+            }
+            const error = await res.json();
+            throw new Error(error.message)
+        }
+
+
+        //if there is data in the request status will be 200 - or 204 if no data
+        if (res.status == 204) {
+            return res;
+        } else {
+            return res.json()
+        }
+    }
+    catch (err) {
+        alert(err.message)
+        throw err;
     }
 }
---
+++ src/api/views/util.js
@@ -0,0 +1,11 @@
+export function getUserData(){
+
+}
+
+export function setUserData(){
+    
+}
+
+export function clearUserData(){
+    
+}
