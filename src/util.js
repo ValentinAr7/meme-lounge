46231b5664e01551458c7022f9cc00e7727af747
Valentin <valio990210@gmail.com>
2023-03-08 20:27:38 +0200

    c.

--- src/util.js
+++ src/util.js
@@ -1,12 +1,15 @@
+// Get the user data from the session storage
 export function getUserData(){
     const data = sessionStorage.getItem('userData')
     return data;
 }
 
+// Set the user data in the session storage
 export function setUserData(data){
     sessionStorage.setItem('userData', JSON.stringify(data))
 }
 
+// Clear the user data from the session storage
 export function clearUserData(){
     sessionStorage.removeItem('userData')
 }
