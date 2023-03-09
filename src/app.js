6f2692493e4b84e1f53bcda04093a0068b2c5545
Valentin <valio990210@gmail.com>
2023-03-09 13:23:01 +0200

    decorate ctx

--- src/app.js
+++ src/app.js
@@ -1,3 +1,26 @@
-import * as api from './api/users.js'
+import { page, render } from './lib.js'
 
-window.api = api
+const main = document.querySelector('main')
+
+page(decorateContext);
+page('/', () => console.log('home'));
+page('/memes', () => console.log('catalog'));
+page('/memes/:id', () => console.log('details'));
+page('/edit/:id', () => console.log('edi'));
+page('/login', () => console.log('login'));
+page('/register', () => console.log('register'));
+page('/create', () => console.log('create'));
+page('/profile', () => console.log('profile'));
+
+
+page.start()
+
+function decorateContext(ctx, next) {
+    ctx.render = renderMain;
+
+    next()
+}
+
+function renderMain(templateResult) {
+
+}
