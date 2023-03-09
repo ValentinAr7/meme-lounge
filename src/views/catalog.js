2725371ac0f6e074171ac11d3f60340977d24bf6
Valentin <valio990210@gmail.com>
2023-03-09 13:59:22 +0200

    catalog view

--- src/views/catalog.js
+++ src/views/catalog.js
@@ -1,9 +1,36 @@
+import { getAllMemes } from '../api/memes.js'
 import { html } from '../lib.js'
 
 
-const catalogTemplate = () => html `
+const catalogTemplate = (memes) => html`
+<section id="meme-feed">
+<h1>All Memes</h1>
+
+    <div id="memes">
+    ${memes.length == 0
+        ? html`<p class="no-memes">No memes in database.</p>`
+        : memes.map(memeCard)}
+        </div>
+        </section>`;
+
+const memeCard = (meme) => html`
+<div id="memes">
+    <!-- Display : All memes in database ( If any ) -->
+    <div class="meme">
+        <div class="card">
+            <div class="info">
+                <p class="meme-title">Debugging</p>
+                <img class="meme-image" alt="meme-img" src="/images/2.png">
+            </div>
+            <div id="data-buttons">
+                <a class="button" href="#">Details</a>
+            </div>
+        </div>
+    </div>
 `
 
-export function catalogView(ctx){
-    ctx.render(catalogTemplate())
+export async function catalogView(ctx) {
+    const memes = await getAllMemes()
+
+    ctx.render(catalogTemplate(memes))
 }
