8d5d8c28f46a7a9379f6eec96e200895edc0c612
Valentin <valio990210@gmail.com>
2023-03-09 18:33:39 +0200

    retrieve memes

--- src/api/memes.js
+++ src/api/memes.js
@@ -3,3 +3,7 @@ import { get } from './api.js'
 export async function  getAllMemes(){
     return get('/data/memes?sortBy=_createdOn%20desc')
 }
+
+export async function createMeme(){
+    return post('/data/memes', meme)
+}
