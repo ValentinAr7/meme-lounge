2e4c29eb4a905db151b55aa09df854c25d42c1e0
Valentin <valio990210@gmail.com>
2023-03-10 11:55:56 +0200

    delete btn

--- src/views/details.js
+++ src/views/details.js
@@ -1,9 +1,9 @@
-import { getAllMemes, getMemeById } from '../api/memes.js'
+import { deleteMeme, getAllMemes, getMemeById } from '../api/memes.js'
 import { html, } from '../lib.js'
 import { getUserData } from '../util.js'
 
 
-const detailsTemplate = (meme, isOwner) => html`
+const detailsTemplate = (meme, isOwner, onDelete) => html`
 <section id="meme-details">
 <h1>Meme Title: ${meme.title}
 </h1>
@@ -19,7 +19,7 @@ const detailsTemplate = (meme, isOwner) => html`
 
         ${isOwner ? html `        <!-- Buttons Edit/Delete should be displayed only for creator of this meme  -->
         <a class="button warning" href="/edit/${meme._id}">Edit</a>
-        <button class="button danger">Delete</button>` : ''}
+        <button  @click=${onDelete}class="button danger">Delete</button>` : ''}
 
         
     </div>
@@ -31,5 +31,14 @@ export async function detailsView(ctx) {
     const meme = await getMemeById(ctx.params.id)
     const userData = getUserData();
     const isOwner = userData?.id == meme._ownerId;
-    ctx.render(detailsTemplate(meme, isOwner))
+    ctx.render(detailsTemplate(meme, isOwner, onDelete))
+
+    async function onDelete(){
+        const choice = confirm('Are you sure you want to delete this meme')
+
+        if(choice){
+            await deleteMeme(ctx.params.id)
+            ctx.page.redirect('/memes')
+        }
+    }
 }
