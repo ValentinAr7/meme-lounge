67e1acd7b2467e33d690794e633c92bcc8f0594d
Valentin <valio990210@gmail.com>
2023-03-11 18:23:23 +0200

    .

--- src/util.js
+++ src/util.js
@@ -1,11 +1,11 @@
-// Get the user data from the session storage
-export function getUserData() {
-    return JSON.parse(sessionStorage.getItem('userData'))
-}
-
 // Set the user data in the session storage
 export function setUserData(data) {
-    sessionStorage.setItem('userData', JSON.stringify(data))
+    sessionStorage.setItem('userData', JSON.stringify(data))    //convert data into string
+}
+
+// Get the user data from the session storage
+export function getUserData() {
+    return JSON.parse(sessionStorage.getItem('userData'))       //convert data back to obj
 }
 
 // Clear the user data from the session storage
